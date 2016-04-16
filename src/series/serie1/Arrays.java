ce2aac9eb4d8f5987172bfbf18b6cd930fca842f
mvcorrea <mvcorrea@gmail.com>
2016-04-16 10:43:23 +0100

    count equals change

--- src/series/serie1/Arrays.java
+++ src/series/serie1/Arrays.java
@@ -6,29 +6,29 @@ package series.serie1;
 public class Arrays {
 
 
-//    public static int removeIndexes(int[] v1, int l1, int r1, int[] v2, int l2, int r2) {
-//        int put = l1-1, get = l1;
-//        while (get <= r1) {
-//            int l = l2, r = r2;
-//            boolean found = false;
-//            while (l <= r && !found) {
-//                int mid = (l + r) / 2;
-//                if (v1[get] == v2[mid]){
-//                    found = true;
-//                } else {
-//                    if (v1[get] < v2[mid]){
-//                        r = mid - 1;
-//                    } else {
-//                        l = mid + 1;
-//                    }
-//                }
-//            }
-//            if (!found){ v1[++put] = v1[get]; }
-//            get++;
-//        }
-//        return put + 1;
-//    }
-
+    /*   public static int removeIndexes(int[] v1, int l1, int r1, int[] v2, int l2, int r2) {
+        int put = l1-1, get = l1;
+        while (get <= r1) {
+            int l = l2, r = r2;
+            boolean found = false;
+            while (l <= r && !found) {
+                int mid = (l + r) / 2;
+                if (v1[get] == v2[mid]){
+                    found = true;
+                } else {
+                    if (v1[get] < v2[mid]){
+                        r = mid - 1;
+                    } else {
+                        l = mid + 1;
+                    }
+                }
+            }
+            if (!found){ v1[++put] = v1[get]; }
+            get++;
+        }
+        return put + 1;
+    }
+    */
 
     public static int removeIndexes(int[] v1, int l1, int r1, int[] v2, int l2, int r2) {
 
@@ -45,6 +45,19 @@ public class Arrays {
     }
 
 
+     /*   public static int countEquals(int[] v1, int l1, int r1, int[] v2, int l2, int r2) {
+        int count = 0;
+        while (l1 <= r1 && l2 <= r2) {
+            if (v1[l1] > v2[l2]) l2++;
+            else if (v1[l1] < v2[l2]) l1++;
+            else {
+                count++;
+                l1++;
+                l2++;
+            }
+        }
+        return count;
+    }*/
 
     public static int countEquals(int[] v1, int l1, int r1, int[] v2, int l2, int r2) {
         int count = 0;
@@ -62,19 +75,7 @@ public class Arrays {
 
 
 
- /*   public static int countEquals(int[] v1, int l1, int r1, int[] v2, int l2, int r2) {
-        int count = 0;
-        while (l1 <= r1 && l2 <= r2) {
-            if (v1[l1] > v2[l2]) l2++;
-            else if (v1[l1] < v2[l2]) l1++;
-            else {
-                count++;
-                l1++;
-                l2++;
-            }
-        }
-        return count;
-    }*/
+
 
 
     public static String greaterCommonPrefix(String[] v, int l, int r, String word){
